0a10af98e2150dee4ab7e32c9ea4b7ec865acb55
anniepan21 <32742883+anniepan21@users.noreply.github.com>
2017-10-12 11:51:24 -0500

    Update decisiontrees.R

--- decisiontrees.R
+++ decisiontrees.R
@@ -1,9 +1,10 @@
 ### Lab – Chapter 5 Divide and Conquer – Classification Using Decision Trees and Rules
 
-disease <- read.csv("U.S._Ch
-ronic_Disease_Indicators__CDI_processed.csv")
+### Read data
+disease <- read.csv("U.S._Chronic_Disease_Indicators__CDI_processed.csv")
 str(disease)
 
+### Split data into train and test
 set.seed(123)
 test_sample <- sample(12979,2979)
 str(test_sample)
@@ -11,6 +12,7 @@ str(test_sample)
 train <- disease[-test_sample,]
 test <- disease[test_sample,]
 
+### Extract labels
 train_lbl <- train[,7]
 test_lbl <- test[,7]
 
@@ -20,14 +22,17 @@ prop.table(table(test$Stratification))
 train <- train[,-7]
 test <- test[,-7]
 
+### Train the C5.0 decision tree model
 model <- C5.0(train, train_lbl)
 summary(model)
 
+### Test the model
 pred <- predict(model,test)
 library(gmodels)
 CrossTable(test_lbl, pred, prop.chisq=FALSE, prop.c=FALSE, prop.r=FALSE, dnn=c('actual default','predicted default'))
 
-model_2 <- C5.0(train, train_lbl, trails = 2)
+### Increase "trails" parameter to value between 2 and 10
+model_2 <- C5.0(train, train_lbl, trails = 5)
 summary(model_2)
 
 pred_2 <- predict(model_2,test)
